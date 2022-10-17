924b15b1706f7dd2a587ee0ded11cb665bc316e4
Derreck <derreck775@gmail.com>
2022-10-17 10:51:23 -0300

    b

--- processos/app.py
+++ processos/app.py
@@ -1,10 +1,15 @@
-from processos.ext import rotas
 from flask import Flask
 
-app = Flask(__name__)
+from processos.ext import config
 
 
-rotas.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+    config.init_app(app)
+    return app
+
+
 
 
 
