2b86b2b167819278631320e96a9624dff73d5d0e
Derreck <derreck775@gmail.com>
2022-10-18 10:20:26 -0300

    c

--- processos/ext/rotas.py
+++ processos/ext/rotas.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 example_blueprint = Blueprint('example_blueprint', __name__)
 
+@example_blueprint.route('/index')
 @example_blueprint.route('/')
 def index():
-    return "This is an example app"
+    return render_template("index.html")
 
 def init_app(app):
     app.register_blueprint(example_blueprint)
